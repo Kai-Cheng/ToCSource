8a5d1bdd001578ded00e439b62bf4794faa3db09
KCW <kcw@sbarcotech.com>
2018-04-11 11:58:35 +0800

    Use the file name to define the array name.

--- ToCSource/ToCSource.cpp
+++ ToCSource/ToCSource.cpp
@@ -5,12 +5,68 @@
 #include "stdlib.h"
 #include <windows.h>
 
-_TCHAR *get_filename_extT(_TCHAR* filename) {
+_TCHAR *get_filename_extT(_TCHAR* filename)
+{
 	_TCHAR *dot = _tcsrchr(filename, _T('.'));
 	if (!dot || dot == filename) return NULL;
 	return dot + 1;
 }
 
+_TCHAR *get_filename(_TCHAR* filename)
+{
+	_TCHAR *dot = _tcsrchr(filename, _T('.'));
+	_TCHAR *file = filename;
+	if (!dot || dot == filename) return NULL;
+
+	_TCHAR *tPath = _tcsrchr(filename, _T('\\'));
+	if (tPath)
+		file = tPath;
+
+	return file + 1;
+}
+
+void str_Replace(char* tStr, char tCharNew)
+{
+	int tStrLen = strlen(tStr);
+	if (!((tCharNew >= 'A' && tCharNew <= 'Z') ||
+		(tCharNew >= 'a' && tCharNew <= 'z') ||
+		(tCharNew >= '0' && tCharNew <= '9') ||
+		(tCharNew == '_')))
+		tCharNew = '_';
+	for (int i = 0; i < tStrLen && tStr[i]; i++)
+	{
+		if (!((tStr[i] >= 'A' && tStr[i] <= 'Z') ||
+			(tStr[i] >= 'a' && tStr[i] <= 'z') ||
+			(tStr[i] >= '0' && tStr[i] <= '9') ||
+			(tStr[i] == '_')))
+			tStr[i] = tCharNew;
+	}
+	if ((tStr[0] >= '0' && tStr[0] <= '9'))
+		tCharNew = '_';
+	return;
+}
+/*
+void tcs_Replace(_TCHAR* tStr, _TCHAR tCharNew)
+{
+	int tStrLen = _tcslen(tStr);
+	if (!((tCharNew >= _T('A') && tCharNew <= _T('Z')) ||
+		(tCharNew >= _T('a') && tCharNew <= _T('z')) ||
+		(tCharNew >= _T('0') && tCharNew <= _T('9')) ||
+		(tCharNew == _T('_'))))
+		tCharNew = _T('_');
+	for (int i = 0; i < tStrLen && tStr[i]; i++)
+	{
+		if (!((tStr[i] >= _T('A') && tStr[i] <= _T('Z')) ||
+			(tStr[i] >= _T('a') && tStr[i] <= _T('z')) ||
+			(tStr[i] >= _T('0') && tStr[i] <= _T('9')) ||
+			(tStr[i] == _T('_'))))
+			tStr[i] = tCharNew;
+	}
+	if ((tStr[0] >= _T('0') && tStr[0] <= _T('9')))
+		tCharNew = _T('_');
+	return;
+}
+*/
 int _tmain(int argc, _TCHAR* argv[])
 {
 	FILE *pfile;
@@ -28,6 +84,7 @@ int _tmain(int argc, _TCHAR* argv[])
 #if _DEBUG//[.4test.
 	argc = 2;
 	argv[0] = _T("ToCSource.exe");
+	//argv[1] = _T("TT\\+ W23& H23_R.raw");
 	argv[1] = _T("+ W23& H23_R.raw");
 	//argv[2] = _T("2");
 	if (argc != 2)
@@ -38,7 +95,7 @@ int _tmain(int argc, _TCHAR* argv[])
 #else
 	if (argc == 2 && (!_tcscmp(argv[1], _T("-?")) || !_tcscmp(argv[1], _T("-h")) || !_tcscmp(argv[1], _T("-help"))))
 	{
-		printf("Version 1.00\r\n");
+		printf("Version 1.0.0.1\r\n");
 		_tprintf(TEXT("\nUsage: %s <file path>\n"), argv[0]);
 		//printf("This program built for Win8.1x64\n");
 		printf("Report error to kai.cheng.wang@gmail.com\n");
@@ -53,7 +110,20 @@ int _tmain(int argc, _TCHAR* argv[])
 #endif//].
 
 	_tcscpy(newDir, argv[1]);
+	TCHAR *tFile;
+	char cFile[MAX_PATH];
+	int n, len;
+
 	pDir = get_filename_extT(newDir);
+	tFile = get_filename(newDir);
+	memset(cFile, 0, sizeof(cFile));
+	len = (pDir  - tFile) - 1;
+	for (n = 0; n < len; n++)
+	{
+		cFile[n] = (char)tFile[n];
+	}
+
+	str_Replace(cFile, '_');
 	_tcscpy(pDir, _T("c")); // *.c
 
 	// Open file and reading data to pBuf
@@ -90,6 +160,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	unsigned long  cSrcdataLLen = 0;
 	unsigned long  cSrcdataSize = 35;
 
+	cSrcdataSize += strlen(cFile);
 	cSrcdataSize += (datasize * 6); // "0xFF, "
 	cSrcdataSize += ((datasize + 15) / 16) * 3; // "\t\r\n"
 
@@ -101,10 +172,14 @@ int _tmain(int argc, _TCHAR* argv[])
 		printf("Malloc memory fail [C].");
 		return (-1);
 	}
-	strcpy(pCSrcbuf, "unsigned char data[");
+	//strcpy(pCSrcbuf, "unsigned char data[");
+	strcpy(pCSrcbuf, "unsigned char ");
+	strcat(pCSrcbuf, cFile);
+	strcat(pCSrcbuf, "[");
 	_itoa(datasize, dLenStr, 10);
 	strcat(pCSrcbuf, dLenStr);
 	strcat(pCSrcbuf, "] =\r\n{");
+
 	for (i = 0, j = 1; i < datasize; i++, j++)
 	{
 		if ((i % 16) == 0)
@@ -133,7 +208,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			strcat(pCSrcbuf, ", ");
 		}
 	}
-	strcat(pCSrcbuf, "\r\n};");
+	strcat(pCSrcbuf, "\r\n};\r\n");
 	cSrcdataLLen = strlen(pCSrcbuf);
 
 	/* open file with w+b and write pBuf to file */
